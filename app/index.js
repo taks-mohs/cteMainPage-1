a90da3435765524e1980a941b11f5d2cac794679
hellotheretm <qwertygoofy.123@gmail.com>
2026-01-27 13:57:37 -1000

    image buttons 1/27

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, ImageBackground, ScrollView, useWindowDimensions, Image, Pressable } from 'react-native'
+import { StyleSheet, View, ImageBackground, ScrollView, useWindowDimensions, Image, Pressable, Button } from 'react-native'
 import React, { useState, useEffect } from 'react';
 import { useFonts, Oswald_300Light, Oswald_600SemiBold, Oswald_500Medium } from '@expo-google-fonts/oswald'
 import Colors from '../components/colors'
@@ -17,16 +17,22 @@ export default function HomePage() {
       require('../assets/CS/MiscImages/CSbackground.jpeg')
    ]
    const [currentBackground, setCurrentBackground] = useState(backgrounds[0])
-   const [index, setIndex] = useState(0);
+   const [index, setIndex] = useState(0)
+   const [currentStyle, setCurrentStyle] = useState(styles.imgBtn)
    const { width } = useWindowDimensions()
 
    useEffect(() => {
       const interval = setInterval(() => {
          setIndex((prevIndex) => (prevIndex + 1) % backgrounds.length)
+         setCurrentBackground(backgrounds[index])
       }, 5000)
       return () => clearInterval(interval)
 
    }, [])
+
+   updateButtons = () =>{
+      
+   }
 
    useFonts({
       'oswaldlight': Oswald_300Light,
@@ -47,14 +53,32 @@ export default function HomePage() {
       img: {
          height: 500,
          width: '100%',
-         justifyContent: 'center'
+         justifyContent: 'flex-end',
+         alignItems: 'flex-start',
 
       },
+      btnContainer: {
+         height: 40,
+         width: width,
+         padding: 10,
+         flexDirection: 'row',
+         columnGap: 5
+      },
       imgBtn: {
-         height: 50,
-         width: 50,
-         borderRadius: 25,
-         color: 'black'
+         height: 20,
+         width: 20,
+         borderRadius: 10,
+         borderWidth: 2,
+         borderColor: 'black',
+         backgroundColor: '#ffffff23'
+      },
+      btnPressed: {
+         height: 20,
+         width: 20,
+         borderRadius: 10,
+         borderWidth: 2,
+         borderColor: 'black',
+         backgroundColor: '#000000'
       }
    })
 
@@ -62,11 +86,21 @@ export default function HomePage() {
       <View style={styles.background}>
          <TopBar />
          <ScrollView>
-            <ImageBackground style={styles.img} source={backgrounds[index]}>
-               <Pressable
-                  style={styles.imgBtn}
-               > 
-               </Pressable>
+            <ImageBackground style={styles.img} source={currentBackground}>
+               <View style={styles.btnContainer}>
+                  {backgrounds.map((imageUri) => {
+                     return (
+                        <Pressable
+                           style={({ pressed }) => [
+                              styles.btnPressed,
+                              styles.imgBtn
+                           ]}
+
+                        >
+                        </Pressable>
+                     )
+                  })}
+               </View>
             </ImageBackground>
             <View style={{ marginTop: width * 0.25 }}></View>
             <View style={{ marginTop: width * 0.05 }}></View>
